b423bdb6de3911f506af41a462d2ac9d8365e6bc
nbadalls <1277046020@qq.com>
2018-05-24 15:50:11 +0800

    rm folder jobs

--- jobs/AMImageCdata/AMImageCdata-b0.35s30_fc_0.35_112x96_b+FaceAdd_faceNet-20-light2s4-bn/2018-05-23_AMImageCdata-b0.35s30_fc_0.35_112x96_b+FaceAdd_faceNet-20-light2s4-bn_zkx.sh
+++
@@ -1,6 +0,0 @@
-cd /home/zkx/Project/O2N/caffe-master
-./build/tools/caffe train \
---solver="models/AMImageCdata/AMImageCdata-b0.35s30_fc_0.35_112x96_b+FaceAdd_faceNet-20-light2s4-bn/solver.prototxt" \
---gpu="0,1,2,3" \
---snapshot="../asset/snapshot/AMImageCdata/AMImageCdata-b0.35s30_fc_0.35_112x96_b+FaceAdd_faceNet-20-light2s4-bn/2018-05-22_AMImageCdata-b0.35s30_fc_0.35_112x96_b+FaceAdd_faceNet-20-light2s4-bn_zkx_iter_80000.solverstate" \
-2>&1 | tee jobs/AMImageCdata/AMImageCdata-b0.35s30_fc_0.35_112x96_b+FaceAdd_faceNet-20-light2s4-bn/2018-05-23_AMImageCdata-b0.35s30_fc_0.35_112x96_b+FaceAdd_faceNet-20-light2s4-bn_zkx.log
--- jobs/AdditMarginLmdbNoE/AdditMarginLmdbNoE-b0.35s30_fc_0.35_112x96_faceNet-20-light2s4-bn/2018-05-22_AdditMarginLmdbNoE-b0.35s30_fc_0.35_112x96_faceNet-20-light2s4-bn_zkx.sh
+++
@@ -1,6 +0,0 @@
-cd /home/zkx/Project/O2N/caffe-master
-./build/tools/caffe train \
---solver="models/AdditMarginLmdbNoE/AdditMarginLmdbNoE-b0.35s30_fc_0.35_112x96_faceNet-20-light2s4-bn/solver.prototxt" \
---gpu="0,1,2,3" \
---snapshot="../asset/snapshot/AdditMarginLmdbNoE/AdditMarginLmdbNoE-b0.35s30_fc_0.35_112x96_faceNet-20-light2s4-bn/2018-05-22_AdditMarginLmdbNoE-b0.35s30_fc_0.35_112x96_faceNet-20-light2s4-bn_zkx_iter_400000.solverstate" \
-2>&1 | tee jobs/AdditMarginLmdbNoE/AdditMarginLmdbNoE-b0.35s30_fc_0.35_112x96_faceNet-20-light2s4-bn/2018-05-22_AdditMarginLmdbNoE-b0.35s30_fc_0.35_112x96_faceNet-20-light2s4-bn_zkx.log
