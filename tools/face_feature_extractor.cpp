f619389402e509808eca25b9a1d206c0b1db0277
nbadalls <1277046020@qq.com>
2018-05-31 14:42:47 +0800

    fix some bug

--- tools/face_feature_extractor.cpp
+++ tools/face_feature_extractor.cpp
@@ -12,16 +12,16 @@ using namespace caffe;
 int main(int argc ,char* argv[])
   {
 
-//    FLAGS_alsologtostderr = 1;  // Print output to stderr (while still logging)
-//    ::google::InitGoogleLogging(argv[0]);
-//    if(argc < 2)
-//    {
-//        std::cout << "Please input Configuration file path"<<std::endl;
-//        return -1;
-//    }
+   FLAGS_alsologtostderr = 0;  // Print output to stderr (while still logging)
+   ::google::InitGoogleLogging(argv[0]);
+   if(argc < 2)
+   {
+       std::cout << "Please input Configuration file path"<<std::endl;
+       return -1;
+   }
 
-//    string config_path = argv[1];
-    string config_path = "/home/minivision/SoftWare/caffe-server/scripts/face_feature_extractor/param.prototxt";
+   string config_path = argv[1];
+    // string config_path = "/home/minivision/SoftWare/caffe-server/scripts/face_feature_extractor/param.prototxt";
     Extract_Feature feature_extractor(config_path);
     feature_extractor.ImagePairExtractFeature();
     return 0;
