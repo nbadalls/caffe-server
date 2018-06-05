4bb699dec0486fb8c587f8f8a33686fcb0bb43ff
nbadalls <1277046020@qq.com>
2018-06-05 19:55:17 +0800

    update

--- python/train_set.py
+++ python/train_set.py
@@ -4,9 +4,10 @@
 #FaceAdd -- ZheD-_Cmul-Asia-beid-cap10
 #MS -- MSceleb 
 
-image_root_folder = "/home/zkx/Data_sdb/TrainData/Data_sdc/Patches/MultiPatches"
-label_root_path = "/home/zkx/Data_sdb/TrainData/Data_sdc/Patches/MultiPatches/image_list/train_list"
+image_root_folder = "/home/hjg/Data/SDB_Disk/Data/Train_Data/O2N/Data_sdc/Patches/MultiPatches"
+label_root_path = "/home/hjg/Data/SDB_Disk/Data/Train_Data/O2N/Data_sdc/Patches/MultiPatches/image_list/train_list"
 train_img_data = {
+        'b+FaceAdd+Mobile': ["{}/Combine_base_Cmul-Asia-beid-cap10-ZheD-CHAMob_list.txt_label.txt".format(label_root_path),55918],
 	'b+FaceAdd': [ "{}/Combine_base_Cmul-Asia-beid-cap10-ZheD_list.txt_label.txt".format(label_root_path), 54547], 
 	'base' :['{}/shot_Oriental_Age_Lan_DHUA_PAKJ_Indon_Migrant_celeb_label.txt'.format(label_root_path), 21331],
 }
